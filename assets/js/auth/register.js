c4adcf4ebc9153628882c6384e15220050c1cb2d
MariaJessLica2003 <mariajessamie.licaros@carsu.edu.ph>
2023-12-21 20:17:45 +0800

    Added registration functionality

--- assets/js/auth/register.js
+++ assets/js/auth/register.js
@@ -1,4 +1,4 @@
-//Form Register
+s//Form Register
 
 const form_register = document.getElementById('form_register');
 
@@ -17,7 +17,7 @@ form_register.onsubmit = async (e) => {
     const formData = new FormData(form_register);
 
 //fetch api user register
-    const response = await fetch ("f19a-2001-4455-5c8-1f00-c50d-c28d-2d0b-ae4c.ngrok-free.app" + "/api/user"
+    const response = await fetch ("http://realestate.test/api/user" + "/api/user"
         , {
             method: "POST",
             header: {
@@ -26,21 +26,55 @@ form_register.onsubmit = async (e) => {
             body:formData,
         });
 
-        //get response if 200-200 status code
+        //get response if 200-299 status code
         if (response.ok) {
             const json = await response.json();
             console.log(json);
 
+
+            document.querySelector(".alert-success").classList.remove('d-none');
+            document.querySelector(".alert-success").classList.add('d-block');
+
+            
+        
             form_register.reset();
+
+            successNotification("Successfully registered account", 5);
         }
         //get response if 422 status code
         else if (response.status ==422){
             const json = await response.json();
 
-            alert(json.message);
+        
+            errorNotification(json.message, 5);
         }
 
         //enable button
         document.querySelector("#form_register button").disabled = false;
         
+};
+
+
+
+function successNotification(message, seconds){
+    document.querySelector(".alert-success").classList.remove('d-none');
+    document.querySelector(".alert-success").classList.add('d-block');
+    document.querySelector(".alert-success").innerHTML=message;
+
+
+    setTimeout(function(){
+        document.querySelector(".alert-success").classList.remove('d-block');
+    document.querySelector(".alert-success").classList.add('d-none');
+    },seconds * 1000);
+
+}
+function errorNotification(message, seconds){
+    document.querySelector(".alert-danger").classList.remove('d-none');
+    document.querySelector(".alert-danger").classList.add('d-block');
+    document.querySelector(".alert-danger").innerHTML=message;
+
+    setTimeout(function(){
+        document.querySelector(".alert-danger").classList.remove('d-block');
+    document.querySelector(".alert-danger").classList.add('d-none');
+    }, seconds * 1000);
 }
